f7d3d286e7db684b7f6f5a74c340a588ffcee771
Kunwar Saluja <saluja@adobe.com>
2024-05-09 14:54:30 -0400

    cards block

---
+++ blocks/cards/cards.js
@@ -0,0 +1,39 @@
+import { createOptimizedFireflyPicture } from '../../scripts/utils.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length >= 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedFireflyPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
+
+  // Full card should be clickable
+  block.querySelectorAll('.cards > ul > li').forEach((card) => {
+    const anchor = card.querySelector('a');
+    if (anchor && anchor.href) {
+      const alink = anchor.href;
+      card.classList.add('linked-card');
+
+      card.addEventListener('click', () => {
+        document.location.href = alink;
+      });
+    }
+  });
+
+/*   // Make all links in cards, right aligned
+  block.querySelectorAll('.cards .cards-card-body p').forEach((paragraph) => {
+    if (paragraph.querySelector('a')) {
+      paragraph.style.display = 'flex';
+      paragraph.style.justifyContent = 'end';
+    }
+  }); */
+}
