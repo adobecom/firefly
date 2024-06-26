438c986ab0851fcfabee4016691c8d6fa1a4688c
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2024-06-26 14:36:42 -0400

    Card carousel Icon fix (#94)
    * fix
    
    * Update cards-carousel.js

--- aemedge/blocks/cards-carousel/cards-carousel.js
+++ aemedge/blocks/cards-carousel/cards-carousel.js
@@ -13,10 +13,15 @@ let curSlide = 0;
 let maxSlide = 0;
 let filteredSlides = 0;
 
-function scrollToSlide(carouselWrapper, slideIndex) {
+function scrollToSlide(carouselWrapper, slideIndex, dir) {
   const carouselSlider = carouselWrapper.querySelector('.cards-carousel-slide-container');
   const widthtoScroll = (carouselSlider.clientWidth > 767) ? 350 : 170;
-  const leftPos = (widthtoScroll * 2 * slideIndex);
+  let leftPos;
+  if (widthtoScroll === 350) {
+    leftPos = (widthtoScroll * 2 * slideIndex) + (dir === 'prev' ? -50 : 50);
+  } else {
+    leftPos = (widthtoScroll * 2 * slideIndex);
+  }
   carouselSlider.scrollTo({ left: leftPos, behavior: 'smooth' });
   const slides = carouselSlider.children;
   for (let i = 0; i < slides.length; i += 1) {
@@ -48,7 +53,7 @@ function buildNav(dir, carousel) {
       carousel.classList.remove('hide-prev');
       carousel.classList.remove('hide-next');
     }
-    scrollToSlide(carousel, nextSlide);
+    scrollToSlide(carousel, nextSlide, dir);
   });
   return nav;
 }
@@ -72,9 +77,14 @@ function buildSlide(slide, index, featuresArray) {
     }
   }
   filteredSlides += 1;
+  const icon = slide.querySelector('.icon');
+  if (icon) {
+    icon.parentElement.replaceWith(icon);
+  }
   const video = firstDiv.querySelector('a');
   if (video) {
     const image = firstDiv.querySelector('img');
+    const picture = firstDiv.querySelector('picture');
     const videoLink = video.href;
     const videoEl = document.createElement('video');
     videoEl.src = videoLink;
@@ -84,8 +94,8 @@ function buildSlide(slide, index, featuresArray) {
     videoEl.loop = true;
     videoEl.playsinline = true;
     videoEl.className = 'cards-card-video hide';
-    firstDiv.replaceWith(videoEl);
-    slide.prepend(image);
+    video.parentElement.replaceWith(videoEl);
+    picture.parentElement.replaceWith(image);
 
     slide.addEventListener('mouseenter', () => {
       videoEl.classList.remove('hide');
@@ -104,6 +114,12 @@ function buildSlide(slide, index, featuresArray) {
       videoEl.classList.add('hide');
       image.classList.remove('hide');
     });
+    firstDiv.parentElement.prepend(videoEl);
+    firstDiv.parentElement.prepend(image);
+    if (icon) {
+      firstDiv.parentElement.prepend(icon);
+    }
+    firstDiv.remove();
   }
   slide.setAttribute('id', `${SLIDE_ID_PREFIX}${index}`);
   slide.setAttribute('data-slide-index', index);
@@ -116,10 +132,6 @@ function buildSlide(slide, index, featuresArray) {
   linkWrapper.classList.add('card-link');
   linkWrapper.append(link.parentElement);
   slide.append(linkWrapper);
-  const icon = slide.querySelector('.icon');
-  if (icon) {
-    icon.parentElement.replaceWith(icon);
-  }
   const href = link?.href;
   if (href) {
     slide.classList.add('card-with-link');
